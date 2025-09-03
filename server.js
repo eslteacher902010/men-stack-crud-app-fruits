aabbfd4a2f0268f2716f4b42344690f4657ee1f1
paul_kniaz <pjkniaz@gmail.com>
2025-09-03 11:09:44 -0400

    set up dotenv

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require('express');
+//server.js
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-// server.js
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
-// // GET /
-// app.get("/", async (req, res) => {
-//   res.send("hello, friend!");
-// });
+
 
 app.get("/", async (req, res) => {
   res.send("hello, friend!");
